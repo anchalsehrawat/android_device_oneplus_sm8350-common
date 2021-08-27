10cc925802984ffa6d1ffeba1c3b81e20acdfd64
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    sm8350-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- common.mk
+++ common.mk
@@ -266,6 +266,9 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.oplus-multihal \
