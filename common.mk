847bd59c7539077ba58d9df6d7b9a3f9f8461677
anchalsehrawat <anchalsingh987@gmail.com>
2025-08-05 01:57:36 +0530

    sm8350-common: Define TARGET_BOARD_PLATFORM

--- common.mk
+++ common.mk
@@ -245,6 +245,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Platform
+TARGET_BOARD_PLATFORM := lahaina
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
