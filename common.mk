35b74c8ea5757dfd79a50facedbf5c4da2b06786
anchalsehrawat <anchalsingh987@gmail.com>
2025-12-27 13:10:59 +0530

    sm8350-common: Reduce Blur Radius in PixelLauncher
    ref commit: https://github.com/PixelOS-Devices/android_device_xiaomi_sunny/commit/063ab748e31fca56938545706e306d346b9cb0c7
    
    Reduce blur radius while launching apps
    Credit: hiroshi @EvolutionX-Team

--- common.mk
+++ common.mk
@@ -241,7 +241,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
-    WifiResTarget
+    WifiResTarget \
+    LauncherTarget
 
 # Partitions
 PRODUCT_PACKAGES += \
