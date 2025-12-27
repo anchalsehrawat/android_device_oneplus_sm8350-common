b3442e1a9c4316053c0d37c5c07a24b5944804e5
anchalsehrawat <anchalsingh987@gmail.com>
2025-12-27 13:10:59 +0530

    sm8350-common: Reduce Blur Radius
    ref commit: https://github.com/PixelOS-Devices/android_device_xiaomi_sunny/commit/063ab748e31fca56938545706e306d346b9cb0c7

--- common.mk
+++ common.mk
@@ -229,7 +229,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
-    WifiResTarget
+    WifiResTarget \
+    LauncherTarget
 
 # Partitions
 PRODUCT_PACKAGES += \
